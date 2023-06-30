49645b647306be2ec1d83f15bce4cc8cd7f284c0
Michael Ha <77702012+Guacamoley@users.noreply.github.com>
2023-06-29 21:44:54 -0500

    Fix 404 error after clicking on bingo in games

--- games.php
+++ games.php
@@ -18,7 +18,7 @@
     <br>
     <table class="center">
         <tr>
-            <td><a href="/abcd/bingo.php"><img src="https://cdn.onlinewebfonts.com/svg/img_562572.png" id="img"></th></a>
+            <td><a href="bingo.php"><img src="https://cdn.onlinewebfonts.com/svg/img_562572.png" id="img"></th></a>
             <td><a href="quiz.php"><img src="https://cdn3.iconfinder.com/data/icons/brain-games/1042/Quiz-Games-grey.png" id="img"></td></a>
         </tr>
     </table>
