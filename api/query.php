34d741ac468b5045b7456e81e26f6cadf1dd0842
Paul Schmitz <p_schmitz@email.com>
2023-07-10 21:13:16 -0500

    update query api
    updated readme
    added error message to 400 response

--- api/query.php
+++ api/query.php
@@ -7,6 +7,6 @@ try {
     $idList = Dress::getByParams($_GET);
     response(200, count($idList) . " matching results", $idList);
 } catch (mysqli_sql_exception $e) {
-    invalidResponse("Invalid SQL syntax. Parameter names must match database column names.");
+    invalidResponse("Invalid SQL syntax. Note that parameter names must match database column names. Full error details: " . $e);
 }
 ?>
