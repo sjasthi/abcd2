507f485cd30f1898ed6ca573898791b3cf2f7c11
gaug1801 <112595547+gaug1801@users.noreply.github.com>
2023-07-17 21:00:22 -0500

    nav bar in display_the_dress.php
    added nav bar feature in display_the_dress.php. note: navigation is done by id

--- display_the_dress.php
+++ display_the_dress.php
@@ -24,38 +24,70 @@ if (isset($_GET['fav_status'])) {
   }
 }
 
+$id = false;
+
 if (isset($_GET['id'])) {
-
-    $id = mysqli_real_escape_string($db, $_GET['id']);
-    $sql = "SELECT * FROM `dresses` WHERE id = " . $id;
-    $GLOBALS['row_data'] = mysqli_query($db, $sql);
-} 
-else if(isset($_GET['name'])) {
-
-    $name = mysqli_real_escape_string($db, $_GET['name']);
-    $sql = "SELECT * FROM `dresses` WHERE name = '" . $name ."'";
-    $GLOBALS['row_data'] = mysqli_query($db, $sql);
+  $id = mysqli_real_escape_string($db, $_GET['id']);
+} else if(isset($_GET['name'])) {
+  $name = mysqli_real_escape_string($db, $_GET['name']);
+  $sql = "SELECT * FROM `dresses` WHERE name = '" . $name ."'";
+  $result = mysqli_query($db, $sql);
+  if ($result->num_rows > 0){
+      $row = $result->fetch_assoc();
+      $id = $row["id"];
+  }
 }
 
-if ($row_data->num_rows > 0) {
-    // fetch row_data from $_Globals
-    while($row = $row_data->fetch_assoc()) {
-      print( '<div class ="containerTitle"><h2 class= "headTwo">'.$row["name"]. '</h2></div>');
-      print( '<div class ="container">');
-      echo("<div class='containerImage'><image class = 'image' src = images/dress_images/".$row["image_url"]. "></image>");  
-      print( '</div>');  
-      print( '<div class="containerText">
-      <h3 class= "title"><strong> Description: </strong></h3><p class= "words" >'.$row["description"]. '</p>
-      <h3 class= "title"><strong> Did You Know? </strong></h3><p class= "words">' .$row["did_you_know"]. '</p>
-      <h3 class= "title"><strong> Category </strong></h3><p class= "words">' .$row["category"]. '</p>
-      <h3 class= "title"><strong> Type </strong></h3><p class= "words">' .$row["type"]. '</p>
-      <h3 class= "title"><strong> State Name </strong></h3><p class= "words">' .$row["state_name"]. '</p>
-      <h3 class= "title"><strong> Key Words </strong></h3><p class= "words">' .$row["key_words"]. '</p>
-      <h3 class= "title"><strong> Status </strong></h3><p class= "words">' .$row["status"]. '</p>
-      <h3 class= "title"><strong> Notes </strong></h3><p class= "words">' .$row["notes"]. '</p></div>' );
-      echo( '</div>');
-    }
-} else {
+if ($id){
+  $minMaxSql = "SELECT MIN(id) as min_id, MAX(id) as max_id FROM `dresses`";
+  $minMaxResult = mysqli_query($db, $minMaxSql);
+  $minMaxRow = $minMaxResult->fetch_assoc();
+  $min_id = $minMaxRow["min_id"];
+  $max_id = $minMaxRow["max_id"];
+
+  $prevSql = "SELECT id FROM `dresses` WHERE id < $id ORDER BY id DESC LIMIT 1";
+  $nextSql = "SELECT id FROM `dresses` WHERE id > $id ORDER BY id ASC LIMIT 1";
+
+  $prevResult = mysqli_query($db, $prevSql);
+  $nextResult = mysqli_query($db, $nextSql);
+
+  $prev_id = ($prevResult->num_rows > 0) ? $prevResult->fetch_assoc()["id"] : $max_id;
+  $next_id = ($nextResult->num_rows > 0) ? $nextResult->fetch_assoc()["id"] : $min_id;
+
+  $sql = "SELECT * FROM `dresses` WHERE id = " . $id;
+  $row_data = mysqli_query($db, $sql);
+
+  if ($row_data->num_rows > 0) {
+      // fetch row_data from $_Globals
+      while($row = $row_data->fetch_assoc()) {
+        print( '<div class ="containerTitle"><h2 class= "headTwo">'.$row["name"]. '</h2></div>');
+        print('<div class="pageNavContainer">
+        <tr class="pageNav">
+          <td> <a class="pageLink pageButton" href="display_the_dress.php?id='.$min_id.'"><< First</a></td>
+          <td> <a class="pageLink pageButton" href="display_the_dress.php?id='.$prev_id.'">Prev</a></td>
+          <td> <a class="pageLink pageButton" href="display_the_dress.php?id='.$next_id.'">Next</a></td>
+          <td> <a class="pageLink pageButton" href="display_the_dress.php?id='.$max_id.'">Last >></a></td>
+        </tr>
+      </div>');
+        print( '<div class ="container">');
+        echo("<div class='containerImage'><image class = 'image' src = images/dress_images/".$row["image_url"]. "></image>");  
+        print( '</div>');  
+        print( '<div class="containerText">
+        <h3 class= "title"><strong> Description: </strong></h3><p class= "words" >'.$row["description"]. '</p>
+        <h3 class= "title"><strong> Did You Know? </strong></h3><p class= "words">' .$row["did_you_know"]. '</p>
+        <h3 class= "title"><strong> Category </strong></h3><p class= "words">' .$row["category"]. '</p>
+        <h3 class= "title"><strong> Type </strong></h3><p class= "words">' .$row["type"]. '</p>
+        <h3 class= "title"><strong> State Name </strong></h3><p class= "words">' .$row["state_name"]. '</p>
+        <h3 class= "title"><strong> Key Words </strong></h3><p class= "words">' .$row["key_words"]. '</p>
+        <h3 class= "title"><strong> Status </strong></h3><p class= "words">' .$row["status"]. '</p>
+        <h3 class= "title"><strong> Notes </strong></h3><p class= "words">' .$row["notes"]. '</p></div>' );
+        echo( '</div>');
+      }
+  } else {
+    print('no data');
+}
+
+ } else {
   print('no data');
 }
 
