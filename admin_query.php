75984713e3f664294c17c7fea33d710feab22ce6
Evan Bjorkstrand <112595947+evanmendo@users.noreply.github.com>
2023-07-24 20:08:26 -0500

    Update admin_query.php
    Fixed search option population and display of ID's once the search is complete

--- admin_query.php
+++ admin_query.php
@@ -17,13 +17,27 @@ include('header.php');
 
 ob_end_flush();
 
-$query = "SELECT DISTINCT category FROM dresses";
+$query = "SELECT key_words FROM dresses";
 $result = mysqli_query($db, $query);
-$categories = $result->fetch_all(MYSQLI_ASSOC);
+$all_keywords = $result->fetch_all(MYSQLI_ASSOC);
 
-$query = "SELECT DISTINCT key_words FROM dresses";
+$query = "SELECT category FROM dresses";
 $result = mysqli_query($db, $query);
-$keywords = $result->fetch_all(MYSQLI_ASSOC);
+$all_categories = $result->fetch_all(MYSQLI_ASSOC);
+
+
+$keywords = [];
+foreach ($all_keywords as $row) {
+    $keywords = array_merge($keywords, explode(',', $row['key_words']));
+}
+
+$categories = [];
+foreach ($all_categories as $row) {
+    $categories = array_merge($categories, explode(',', $row['category']));
+}
+
+$keywords = array_unique($keywords);
+$categories = array_unique($categories);
 
 $result = [];
 
@@ -32,9 +46,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $type = $_POST['type'];
     $keywords = implode(',', $_POST['key_words']);
 
+    $params = [
+        'category' => $category,
+        'type' => $type,
+        'key_words' => $keywords,
+
+    ];
+
     
     
-    $result = Dress::getByParams(array($category), array($type), array($keywords));
+    $result = Dress::getByParams($params);
 }
 ?>
 
@@ -45,28 +66,34 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet">
     <script src="https://kit.fontawesome.com/f40040d297.js" crossorigin="anonymous"></script>
+    <style>
+        #resbox {
+            resize: both;
+            overflow: auto;
+        }
+    </style>
 </head>
 <body>
     <div style="padding: 50px;">
     <form method="POST" action="admin_query.php">
         <label for="category">Category:</label>
         <select multiple name="category[]">
-        <?php foreach($categories as $category): ?>
-            <option value="<?= htmlspecialchars($category['category']) ?>"><?= htmlspecialchars($category['category']) ?></option>
-        <?php endforeach; ?>
+            <?php foreach($categories as $category): ?>
+            <option value="<?= htmlspecialchars($category) ?>"><?= htmlspecialchars($category) ?></option>
+            <?php endforeach; ?>
         </select>
 
         <label for="type">Type:</label>
         <select name="type">
-            <option value="boy">Boy</option>
-            <option value="women">women</option>
-            <option value="other">Other</option>
+            <option value="Boy">Boy</option>
+            <option value="Girl">Girl</option>
+            <option value="Other">Other</option>
         </select>
 
         <label for="key_words">Keywords:</label>
         <select multiple name="key_words[]">
-            <?php foreach($keywords as $key_words): ?>
-                <option value="<?= htmlspecialchars($key_words['key_words']) ?>"><?= htmlspecialchars($key_words['key_words']) ?></option>
+            <?php foreach($keywords as $keyword): ?>
+            <option value="<?= htmlspecialchars($keyword) ?>"><?= htmlspecialchars($keyword) ?></option>
             <?php endforeach; ?>
         </select>
 
@@ -78,7 +105,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <textarea readonly id="resbox">
     <?php if (!empty($result)) { 
         foreach($result as $dress){ 
-          echo ($dress->id . ", ");
+          echo ($dress);
     }
 }
 
