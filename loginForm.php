52489ed26f6030a0d56ec694df85a2e32493d297
Michael Ha <77702012+Guacamoley@users.noreply.github.com>
2023-07-31 19:05:05 -0500

    Fix loginForm.js:1 Uncaught ReferenceError

--- loginForm.php
+++ loginForm.php
@@ -113,7 +113,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
       </div><!-- tab-content -->
 
 </div> <!-- /form -->
-  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
   <script src="js/loginForm.js"></script>
 </body>
 </html>
