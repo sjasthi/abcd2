3f4b05a417ad21272c19a07fd0a1afe0d05506d2
YELIZ KONDUK <43945412+konduyx18@users.noreply.github.com>
2023-11-10 23:45:41 -0600

    Enhancements to Sponsors management
    Enhancements to Sponsors management

--- display_the_sponsor.php
+++ display_the_sponsor.php
@@ -1,5 +1,5 @@
+<!DOCTYPE html>
 <html>
-
 <head>
     <!-- <link href="css/display_the_user.css" rel="stylesheet"> -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -8,41 +8,48 @@
     <link href="css/display_the_sponsor.css" rel="stylesheet">
 </head>
 
-
 <body>
+    <?php
+    include('header.php');
+    include_once 'db_configuration.php';
+    ?>
 
-<?php
-include('header.php');
-include_once 'db_configuration.php';
-?>
+    <div class="ribbon">Sponsor Information</div>
+    <div class="sponsorContainer">
+        <?php
+        if (isset($_GET['id'])) {
+            $id = mysqli_real_escape_string($db, $_GET['id']);
+            $sql = "SELECT * FROM `sponsors` WHERE sponsor_id = " . $id;
+        } else if(isset($_GET['name'])) {
+            $name = mysqli_real_escape_string($db, $_GET['name']);
+            $sql = "SELECT * FROM `sponsors` WHERE name = '" . $name . "'";
+        }
 
-<div class="sponsorContainer">
-<?php
-if (isset($_GET['id'])) {
-    $id = mysqli_real_escape_string($db, $_GET['id']);
-    $sql = "SELECT * FROM `sponsors` WHERE sponsor_id = " . $id;
-    $GLOBALS['row_data'] = mysqli_query($db, $sql);
-} 
-else if(isset($_GET['name'])) {
-    $name = mysqli_real_escape_string($db, $_GET['name']);
-    $sql = "SELECT * FROM `sponsors` WHERE name = '" . $name ."'";
-    $GLOBALS['row_data'] = mysqli_query($db, $sql);
-}
+        $GLOBALS['row_data'] = mysqli_query($db, $sql);
 
-if ($row_data->num_rows > 0) {
-    while($row = $row_data->fetch_assoc()) {
-      print('<h2 class= "head">'.$row["sponsor_id"]. '</h2>'); 
-      print('<h3 class= "title"> Name: </h3><p class= "words">'.$row["name"]. '</p>
-             <h3 class= "title"> Type: </h3><p class= "words">'.$row["type"]. '</p>
-             <h3 class= "title"> Logo: </h3><img src="'.$row["logo"].'" class="thumbnailSize">
-             <h3 class= "title"> Description: </h3><p class= "words">' .$row["description"]. '</p>
-             <h3 class= "title"> Website URL: </h3><a href="'.$row["website_url"].'" target="_blank" class= "words">' .$row["website_url"]. '</a>' );
-    }
-} else {
-    print('no data');
-}
-
-?>
-</div>
+        if ($row_data->num_rows > 0) {
+            while($row = $row_data->fetch_assoc()) {
+                ?>
+                <div class="sponsorSection">
+                    <div class="logoContainer"><img src="<?php echo $row["logo"]; ?>" class="thumbnailSize"></div>
+                    <div class="textContainer">
+                        <h3 class="title">Name:</h3><p class="words"><?php echo $row["name"]; ?></p>
+                        <h3 class="title">Type:</h3><p class="words"><?php echo $row["type"]; ?></p>
+                        <h3 class="title">Description:</h3><p class="words"><?php echo $row["description"]; ?></p>
+                        <h3 class="title">Website URL:</h3><a href="<?php echo $row["website_url"]; ?>" target="_blank" class="words"><?php echo $row["website_url"]; ?></a>
+                    </div>
+                </div>
+                <?php
+            }
+        } else {
+            echo 'No data';
+        }
+        ?>
+    </div>
+    <!-- Footer -->
+    <footer class="page-footer text-center">
+        <p>Created for FA23 ICS 499 ♡</p>
+    </footer>
 </body>
 </html>
+
