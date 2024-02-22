fd5537fb9fbaff1218d759d4328be8e32215d8c5
Dara Phay <130188094+daraP007@users.noreply.github.com>
2024-02-21 20:07:45 -0600

    FP3 iteration Contributors Dara, Faiz, Calvin, and Preston

--- nomination.php
+++ nomination.php
@@ -8,8 +8,8 @@ require 'bin/functions.php';
 require 'db_configuration.php';
 include('header.php');
 
-$page_title = 'Dresses > Create A Dress';
-$page = "create_dress.php";
+$page_title = 'Nomination > Create A Nomination';
+$page = "nomination.php";
 //Check if user is login
 verifyLogin($page);
 
@@ -24,6 +24,9 @@ verifyLogin($page);
     <title>Document</title>
 </head>
 <body>
+
+
+
 <div class="container">
     <style>
         #title {
@@ -36,20 +39,20 @@ verifyLogin($page);
             font-size: 10px;
         }
     </style>
-    <form action="create_the_dress.php" method="POST" enctype="multipart/form-data">
+    <form action="create_nomination.php" method="POST" enctype="multipart/form-data">
         <br>
 
-        <h3 id="title">Nominate</h3> <br>
+        <h3 id="title">Create Your Nomination!</h3> <br>
 
         <div>
         <div>
             <p>Please select the nomination type:</p>
             <div>
-                <input type="radio" id="hero" name="nomination_type" value="Hero" required>
+                <input type="radio" id="hero" name="category" value="Hero" required>
                 <label for="hero">Hero</label>
             </div>
             <div>
-                <input type="radio" id="shero" name="nomination_type" value="Shero" required>
+                <input type="radio" id="shero" name="category" value="Shero" required>
                 <label for="shero">Shero</label>
             </div>
         </div>
@@ -57,14 +60,16 @@ verifyLogin($page);
         </div>
         <div>
             <label>Name</label> <br>
-            <textarea style=width:400px class="form-control" name="description" cols="55" rows="5" required title="Please enter a Name"></textarea>
+            <textarea style=width:400px class="form-control" name="name" cols="55" rows="5" required title="Please enter a Name"></textarea>
         </div>
         <div>
             <label>Description</label> <br>
-            <textarea style=width:400px class="form-control" name="did_you_know" cols="55" rows="2" required title="Please enter a Description"></textarea>
+            <textarea style=width:400px class="form-control" name="description" cols="55" rows="2" required title="Please enter a Description"></textarea>
         </div>
         <br><br>
         <div align="center" class="text-left">
+            <input type= "hidden" id = "email" name ="nominator" value = <?php echo $_SESSION['email'] ?> required> 
+
             <button type="submit" name="submit" class="btn btn-primary btn-md align-items-center">Submit Nomination</button>
         </div>
         <br> <br>
