d902e392792e0376b05764f0105061c70f7b279b
Dara Phay <130188094+daraP007@users.noreply.github.com>
2024-02-21 20:06:31 -0600

    FP3 iteration
    contributor:
    Faiz, Dara, Preston, Calvin

--- nomination.php
+++ nomination.php
@@ -3,16 +3,18 @@ $status = session_status();
 if ($status == PHP_SESSION_NONE) {
     session_start();
 }
-#pulling in helper functions
+
 require 'bin/functions.php';
 require 'db_configuration.php';
 include('header.php');
 
-$page_title = 'Nominations';
-$page = "nomination.php";
-#checking if user is logged in and redirecting to login page if not logged in
+$page_title = 'Dresses > Create A Dress';
+$page = "create_dress.php";
+//Check if user is login
 verifyLogin($page);
 
+?>
+<?php
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -34,12 +36,12 @@ verifyLogin($page);
             font-size: 10px;
         }
     </style>
-    <form action="create_nomination.php" method="POST" enctype="multipart/form-data">
+    <form action="create_the_dress.php" method="POST" enctype="multipart/form-data">
         <br>
-        <br>
-        
+
         <h3 id="title">Nominate</h3> <br>
 
+        <div>
         <div>
             <p>Please select the nomination type:</p>
             <div>
@@ -51,18 +53,16 @@ verifyLogin($page);
                 <label for="shero">Shero</label>
             </div>
         </div>
-        <br>
 
+        </div>
         <div>
             <label>Name</label> <br>
-            <input style=width:400px class="form-control" type="text" name="name" maxlength="100" size="50" required title="Please enter a name"></input>
+            <textarea style=width:400px class="form-control" name="description" cols="55" rows="5" required title="Please enter a Name"></textarea>
         </div>
-
         <div>
             <label>Description</label> <br>
-            <textarea style=width:400px class="form-control" name="description" cols="55" rows="5" required title="Please enter a description"></textarea>
+            <textarea style=width:400px class="form-control" name="did_you_know" cols="55" rows="2" required title="Please enter a Description"></textarea>
         </div>
-
         <br><br>
         <div align="center" class="text-left">
             <button type="submit" name="submit" class="btn btn-primary btn-md align-items-center">Submit Nomination</button>
@@ -72,11 +72,6 @@ verifyLogin($page);
     </form>
 </div>
 
-<script>
-    var loadFile = function(event) {
-        var image = document.getElementById('output');
-        image.src = URL.createObjectURL(event.target.files[0]);
-    };
-</script>
+
 </body>
 </html>
