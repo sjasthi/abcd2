80ac3465a2a8a4d4e8518a51a8ade93cd89eb44e
CwongM <calvinywong@gmail.com>
2024-02-21 18:43:38 -0600

    Adding verifyLogin() check
    Redirects visitors to the login page if they are not logged in as a user or admin

--- nomination.php
+++ nomination.php
@@ -1,3 +1,19 @@
+<?php
+$status = session_status();
+if ($status == PHP_SESSION_NONE) {
+    session_start();
+}
+#pulling in helper functions
+require 'bin/functions.php';
+require 'db_configuration.php';
+include('header.php');
+
+$page_title = 'Nominations';
+$page = "nomination.php";
+#checking if user is logged in and redirecting to login page if not logged in
+verifyLogin($page);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
