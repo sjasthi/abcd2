ab1847e6b58a641bf998c1be9e6826360103cdca
prestofresco <prestonkuhn@gmail.com>
2024-02-23 12:51:56 -0600

    updated nomination form styling and responsiveness

--- nomination.php
+++ nomination.php
@@ -12,67 +12,51 @@ $page_title = 'Nomination > Create A Nomination';
 $page = "nomination.php";
 //Check if user is login
 verifyLogin($page);
-
 ?>
-<?php
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 
+<!-- css styling link -->
+<link href="css/nominationForm.css" rel="stylesheet">
 
-
-<div class="container">
-    <style>
-        #title {
-            text-align: center;
-            color: darkgoldenrod;
-        }
-
-        #guidance {
-            color: grey;
-            font-size: 10px;
-        }
-    </style>
+<div class="container" id="nomination-form-container">
+    
     <form action="create_nomination.php" method="POST" enctype="multipart/form-data">
         <br>
 
-        <h3 id="title">Create Your Nomination!</h3> <br>
+        <h2 id="title">Create Your Nomination!</h2> 
+        <h5 id="subtitle">Please submit this form if you would like to nominate a Shero or Hero.</h5>
+        <br>
 
-        <div>
-        <div>
-            <p>Please select the nomination type:</p>
-            <div>
-                <input type="radio" id="hero" name="category" value="Hero" required>
-                <label for="hero">Hero</label>
+        <label style="font-weight: bold;">Please select the nomination type:</label>
+
+        <div class="form-group">
+            <div class="form-check form-check-inline">
+                <div class="form-check-input">
+                    <input type="radio" id="hero" name="category" value="Hero" required>
+                    <label class="form-check-label" for="hero">Hero</label>
+                </div>
             </div>
-            <div>
-                <input type="radio" id="shero" name="category" value="Shero" required>
-                <label for="shero">Shero</label>
+            <div class="form-check form-check-inline">
+                <div class="form-check-input">
+                    <input type="radio" id="shero" name="category" value="Shero" required>
+                    <label class="form-check-label" for="shero">Shero</label>
+                </div>
             </div>
         </div>
 
+        <div class="form-group">
+            <label for="name" style="font-weight: bold;">Name</label> <br>
+            <input class="form-control" name="name" placeholder="Please enter a Name." required></input>
         </div>
-        <div>
-            <label>Name</label> <br>
-            <textarea style=width:400px class="form-control" name="name" cols="55" rows="5" required title="Please enter a Name"></textarea>
+        <div class="form-group">
+            <label style="font-weight: bold;">Description</label> <br>
+            <textarea class="form-control" name="description" cols="55" rows="5" placeholder="Please enter a Description." required></textarea>
         </div>
-        <div>
-            <label>Description</label> <br>
-            <textarea style=width:400px class="form-control" name="description" cols="55" rows="2" required title="Please enter a Description"></textarea>
-        </div>
-        <br><br>
-        <div align="center" class="text-left">
-            <input type= "hidden" id = "email" name ="nominator" value = <?php echo $_SESSION['email'] ?> required> 
 
-            <button type="submit" name="submit" class="btn btn-primary btn-md align-items-center">Submit Nomination</button>
+        <div class="form-group text-left">
+            <!-- automatically include user's email in the nominator field -->
+            <input type="hidden" id="email" name ="nominator" value= <?php echo $_SESSION['email'] ?>> 
+            <button type="submit" name="submit" class="btn btn-primary btn-md">Submit Nomination</button>
         </div>
-        <br> <br>
 
     </form>
 </div>
