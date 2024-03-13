f9c0dd1555eb9f6a70d880320894a25a33326e4d
CwongM <156030435+Cwong-21@users.noreply.github.com>
2024-03-13 17:44:00 -0500

    Modifying display_nomination.php css
    Altered the css applied to display_nomination.php to match with the styling of display_the_dress.php. This also brings over the navigation to see other nominations without needing to return to the admin view.

--- display_nomination.php
+++ display_nomination.php
@@ -1,36 +1,78 @@
+<?php
+include('header.php');
+include_once 'db_configuration.php';
+?>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="./css/display_the_nomination.css">
+<link rel="stylesheet" href="./css/responsive_style.css">
 <html>
-<head>
-    <link href="css/display_the_resource.css" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Kanit:wght@300&display=swap" rel="stylesheet">
-</head>
 <body>
     <?php
-    include('header.php');
-    include_once 'db_configuration.php';
-    ?>
+    $id = false;
 
-    <div class="container" style="margin-top: 60px;">
-        <?php
-        if (isset($_GET['id'])) {
-            $id = mysqli_real_escape_string($db, $_GET['id']);
-            $sql = "SELECT * FROM `nominations` WHERE id = " . $id;
-            $GLOBALS['row_data'] = mysqli_query($db, $sql);
-        }
+if (isset($_GET['id'])) {
+  $id = mysqli_real_escape_string($db, $_GET['id']);
+} else if(isset($_GET['name'])) {
+  $name = mysqli_real_escape_string($db, $_GET['name']);
+  $sql = "SELECT * FROM `nominations` WHERE name = '" . $name ."'";
+  $result = mysqli_query($db, $sql);
+  if ($result->num_rows > 0){
+      $row = $result->fetch_assoc();
+      $id = $row["id"];
+  }
+}
 
-        if ($row_data->num_rows > 0) {
-            // fetch row_data from $_Globals
-            while($row = $row_data->fetch_assoc()) {
-                echo '<h3 class="title"> Category </h3><p class="words">' .$row["category"]. '</p>';
-                echo '<h3 class="title"> Name </h3><p class="words">' .$row["name"]. '</p>';
-                echo '<h3 class="title"> Description </h3><p class="words">' .$row["description"]. '</p>';
-                echo '<h3 class="title"> Nominator </h3><p class="words">' .$row["nominator"]. '</p>';
-            }
-        } else {
-            echo 'No data';
-        }
-        ?>
-    </div>
+if ($id){
+  $minMaxSql = "SELECT MIN(id) as min_id, MAX(id) as max_id FROM `nominations`";
+  $minMaxResult = mysqli_query($db, $minMaxSql);
+  $minMaxRow = $minMaxResult->fetch_assoc();
+  $min_id = $minMaxRow["min_id"];
+  $max_id = $minMaxRow["max_id"];
+
+  $prevSql = "SELECT id FROM `nominations` WHERE id < $id ORDER BY id DESC LIMIT 1";
+  $nextSql = "SELECT id FROM `nominations` WHERE id > $id ORDER BY id ASC LIMIT 1";
+
+  $prevResult = mysqli_query($db, $prevSql);
+  $nextResult = mysqli_query($db, $nextSql);
+
+  $prev_id = ($prevResult->num_rows > 0) ? $prevResult->fetch_assoc()["id"] : $max_id;
+  $next_id = ($nextResult->num_rows > 0) ? $nextResult->fetch_assoc()["id"] : $min_id;
+
+  $sql = "SELECT * FROM `nominations` WHERE id = " . $id;
+  $row_data = mysqli_query($db, $sql);
+
+  if ($row_data->num_rows > 0) {
+      // fetch row_data from $_Globals
+      while($row = $row_data->fetch_assoc()) { ?>
+        <div class ="containerTitle" style="margin-top: 60px;"><h2 class= "headTwo"><?php echo $row["name"]; ?></h2></div>
+          <div class="pageNavContainer">
+            <tr class="pageNav">
+              <td> <a class="pageLink pageButton" href="display_nomination.php?id=<?php echo $min_id; ?>"><< First</a></td>
+              <td> <a class="pageLink pageButton" href="display_nomination.php?id=<?php echo $prev_id; ?>">Prev</a></td>
+              <td> <a class="pageLink pageButton" href="display_nomination.php?id=<?php echo $next_id; ?>">Next</a></td>
+              <td> <a class="pageLink pageButton" href="display_nomination.php?id=<?php echo $max_id; ?>">Last >></a></td>
+            </tr>
+          </div>
+          <div class ="container">
+            <div class="containerText">
+              <h3 class= "title"><strong> Description: </strong></h3><p class= "words" ><?php echo $row["description"]; ?></p>
+              <h3 class= "title"><strong> Category </strong></h3><p class= "words"><?php echo $row["category"]; ?></p>
+              <h3 class= "title"><strong> Nominator </strong></h3><p class= "words"><?php echo $row["nominator"] ?></p>
+            </div>
+        </div>
+      <?php }
+  } else {
+    print('no data');
+  }
+
+} else {
+  print('no data');
+}
+
+?>
+
 </body>
 </html>
