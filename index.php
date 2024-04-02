f21f8cd9d35a7e18d889ec401d6802cddfdb1d78
CwongM <156030435+Cwong-21@users.noreply.github.com>
2024-04-01 19:00:01 -0500

    Update Shero filter
    Fixed shero filter page functionality

--- index.php
+++ index.php
@@ -124,23 +124,25 @@ echo '<div text-align: left>
     $id_sql = "SELECT `ID` FROM `dresses`";
     $name_sql = "SELECT `name` FROM `dresses`";
     $pic_sql = "SELECT `image_url` FROM `dresses`";
-    
+    $all_sheroes = "743,711,476,733,523,677,688,442,319,473,542,731,115,317,763,468,669,739,690,611,742,724,401,112,313,700,686,560,475,326,735,655,668,710,618,714,578,684,626,703,525,547,671,318,725,32,549,722,434,713,405,728,687,698,691,466,751,435,620,760,102,654,695,768,762,470,605,33,750,114,432,429,439,662,119,265,328,673,30,151,101,493,471,689,31,438,732,323,581,538,324,111,761,723,327,582,506,754,409,440,423,678,588,693,734,692,444,320,664,659,276,658,746,709,534,196,548,117,477,518,418,632,720,445,704,407,426,729,437,748,306,752,577,52,701,50,188,206,441,670,330,771,568,321,740,696,427,766,544,679,699,666,716,322,411,755,764,443,491,737,316,601,685,464,492,53,414,110,676,730,663,753,325,415,356,413,420,39,412,660,736,329,575,469,183,717,463,665,702,27,715,410,425,770,726,430,586,583,28,769,697,406,275,314,574,428,235,681,712,772,483,424,462,201,467,29,741,524,631,718,682,680,758,683,738,465,419,674,472,745,520,474,116,431,721,171,484,436,744,759,672,422,767,433,749,502,756,706,478,719,747,757,26,694,765,667";
+    $shero_query = "SELECT * FROM dresses WHERE id IN ($all_sheroes)";
+
     $input = @$_GET['sort'];
-    if ( strcmp($input,"shero") == 0){
-        $all_sheroes = array(743,711,476,733,523,677,688,442,319,473,542,731,115,317,763,468,669,739,690,611,742,724,401,112,313,700,686,560,475,326,735,655,668,710,618,714,578,684,626,703,525,547,671,318,725,32,549,722,434,713,405,728,687,698,691,466,751,435,620,760,102,654,695,768,762,470,605,33,750,114,432,429,439,662,119,265,328,673,30,151,101,493,471,689,31,438,732,323,581,538,324,111,761,723,327,582,506,754,409,440,423,678,588,693,734,692,444,320,664,659,276,658,746,709,534,196,548,117,477,518,418,632,720,445,704,407,426,729,437,748,306,752,577,52,701,50,188,206,441,670,330,771,568,321,740,696,427,766,544,679,699,666,716,322,411,755,764,443,491,737,316,601,685,464,492,53,414,110,676,730,663,753,325,415,356,413,420,39,412,660,736,329,575,469,183,717,463,665,702,27,715,410,425,770,726,430,586,583,28,769,697,406,275,314,574,428,235,681,712,772,483,424,462,201,467,29,741,524,631,718,682,680,758,683,738,465,419,674,472,745,520,474,116,431,721,171,484,436,744,759,672,422,767,433,749,502,756,706,478,719,747,757,26,694,765,667);
-        
+    //sorting by name alphabetically if shero button is clicked
+    if (strcmp($input,"shero") == 0){
+        $Sort_string = 'name';
     }else{
     $Sort_string = $input;
     }
-        if(empty($Sort_string)) {
-            $Sort_string = 'name' ;
-        }
+    if(empty($Sort_string)) {
+        $Sort_string = 'name';
+    }
 
     
     $id_sql = $id_sql. " ORDER BY " .$Sort_string. " ASC";
     $name_sql = $name_sql. " ORDER BY " .$Sort_string. " ASC";
     $pic_sql = $pic_sql. " ORDER BY " .$Sort_string. " ASC";
-   
+    //this code doesn't do anything with $dresses_results
     $dresses_results = mysqli_query($db, $all_dresses_sql);
     $id_results = mysqli_query($db, $id_sql);
     $name_results = mysqli_query($db, $name_sql);
@@ -164,11 +166,16 @@ echo '<div text-align: left>
             $dress_pics[] = $row;
         }
     }
-
-
-    $total_pages_sql = "SELECT COUNT(*) FROM dresses";
-    $result = mysqli_query($db, $all_dresses_sql);
-    $num_results = mysqli_num_rows($result);
+    //checking for shero filter to alter total page calculation
+    if(strcmp($input,"shero") == 0){
+        $total_pages_sql = "SELECT COUNT(*) FROM dresses WHERE id IN ($all_sheroes)";
+        $result = mysqli_query($db, $shero_query);
+        $num_results = mysqli_num_rows($result);
+    } else {
+        $total_pages_sql = "SELECT COUNT(*) FROM dresses";
+        $result = mysqli_query($db, $all_dresses_sql);
+        $num_results = mysqli_num_rows($result);
+    }
 
     if($dresses_count > $num_results){
         $no_of_records_per_page = 1000000;
@@ -192,24 +199,23 @@ echo '<div text-align: left>
     }
 
     $page_first_result = ($page - 1) * $no_of_records_per_page; 
-   
-
-    $sort_param = isset($_GET['sort']) ? $_GET['sort'] : 'id'; // default to 'id' if no sort param is given
+   //checking for shero filter to change sort param used for SQL Query
+    if(strcmp($input,"shero") == 0) {
+        $sort_param = 'name';
+    } else {
+        $sort_param = isset($_GET['sort']) ? $_GET['sort'] : 'id'; // default to 'id' if no sort param is given
+    }
     $sql = "SELECT * FROM dresses ORDER BY ".$sort_param." ASC LIMIT " . $page_first_result . ',' . $no_of_records_per_page;
 
  
-    if(!empty($all_sheroes)){
-        $shero_query = implode(",", $all_sheroes);
-        $sql = "SELECT * FROM dresses WHERE id IN ($shero_query) ORDER BY CASE id ";
-        foreach ($all_sheroes as $index => $id){
-            $sql .= "WHEN $id THEN ". ($index + 1)." ";
-        }
-        $sql .="END;";
-    }
+    
     if(isset($_POST["id"])){
         $sql = "SELECT * FROM dresses ORDER BY id ASC LIMIT " . $page_first_result . ',' . $no_of_records_per_page;
     }
-    if(isset($_POST["name"])){
+    //may need to change this check for POST check shero
+    if(strcmp($input,"shero") == 0){
+        $sql = "SELECT * FROM dresses WHERE id IN ($all_sheroes) ORDER BY name ASC LIMIT " . $page_first_result . ',' . $no_of_records_per_page;
+    } else if(isset($_POST["name"])) {
         $sql = "SELECT * FROM dresses ORDER BY name ASC LIMIT " . $page_first_result . ',' . $no_of_records_per_page;
     }
     if(isset($_POST["state"])){
@@ -246,14 +252,14 @@ echo '<div text-align: left>
     </span>
     <span class="pageNavConatiner">
         <tr class="pageNav">
-            <a class="pageLink pageFirst pageButton" href="?page=1&sort=<?php echo $Sort_string; ?>"><< First</a>
+            <a class="pageLink pageFirst pageButton" href="?page=1&sort=<?php echo $input; ?>"><< First</a>
             <td class="<?php if($page <= 1){ echo 'disabled'; } ?>">
-                <a class="pageLink pageMid pageButton" href="<?php if($page <= 1){ echo '#'; } else { echo "?page=".($page - 1). "&sort=".$Sort_string; } ?>">Prev</a>
+                <a class="pageLink pageMid pageButton" href="<?php if($page <= 1){ echo '#'; } else { echo "?page=".($page - 1). "&sort=".$input; } ?>">Prev</a>
             </td>
             <td class="<?php if($page >= $total_pages_sql){ echo 'disabled'; } ?>">
-                <a class="pageLink pageMid pageButton" href="<?php if($page >= $total_pages){ echo '#'; } else { echo "?page=".($page + 1). "&sort=".$Sort_string; } ?>">Next</a>
+                <a class="pageLink pageMid pageButton" href="<?php if($page >= $total_pages){ echo '#'; } else { echo "?page=".($page + 1). "&sort=".$input; } ?>">Next</a>
             </td>
-                <a class="pageLink pageMid pageLast pageButton" href="?page=<?php echo $total_pages; ?>&sort=<?php echo $Sort_string; ?>">Last >></a>
+                <a class="pageLink pageMid pageLast pageButton" href="?page=<?php echo $total_pages; ?>&sort=<?php echo $input; ?>">Last >></a>
             </tr>
 </span>
 </span>
